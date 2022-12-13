4f5b1220913a19c27ee5cf986c7064fd444ffda1
Philip Basnet <philipbasnet@gmail.com>
2022-12-13 18:42:10 +0545

    feat: add constants

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,3 +1,5 @@
 export const COCKTAILDB_API_BASE_URL: string =
   import.meta.env.VITE_COCKTAILDB_API_BASE_URL ||
   "https://www.thecocktaildb.com/api/json/v1/1";
+export const COCKTAILDB_BASE_URL: string =
+  import.meta.env.VITE_COCKTAILDB_BASE_URL || "https://www.thecocktaildb.com";
