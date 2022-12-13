62d96db6347008a1c9af8f5811f258dc3a2f80b1
Philip Basnet <philipbasnet@gmail.com>
2022-12-13 19:22:21 +0545

    feat: add endpoints

---
+++ src/api/endpoints.ts
@@ -0,0 +1,8 @@
+export const COCKTAILDB_API_SEARCH_ENDPOINT = "/search.php";
+export const COCKTAILDB_API_LOOKUP_ENDPOINT = "/lookup.php";
+export const COCKTAILDB_API_RANDOM_ENDPOINT = "/random.php";
+export const COCKTAILDB_API_RANDOMSELECTION_ENDPOINT = "/randomselection.php";
+export const COCKTAILDB_API_POPULAR_ENDPOINT = "/popular.php";
+export const COCKTAILDB_API_LATEST_ENDPOINT = "/latest.php";
+export const COCKTAILDB_API_LIST_ENDPOINT = "/list.php";
+export const COCKTAILDB_IMAGE_THUMBNAIL_ENDPOINT = "/preview";
